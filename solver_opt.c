d2592d20388f4656fb7430c9c6b6b3037d99cd75
Ioana Profeanu <ioana.profeanu@stud.acs.upb.ro>
2023-05-07 00:35:22 +0300

    test

--- solver_opt.c
+++ solver_opt.c
@@ -41,6 +41,7 @@ double* my_solver(int N, double *A, double* B) {
             for (bk = 0; bk < N; bk += blockSize) {
                 for (i = 0; i < blockSize; i++) {
                     for (j = 0; j < blockSize; j++) {
+						//!!!!!!!!!!!!!!!!!!!!!! vezi cum faci sa tii cont ca e sup triunghiulara
                         for (k = 0; k < blockSize; k++) {
 							AxB[(bi + i) * N + bj + j] += A[(bi + i) * N + bk + k] * B[(bk + k) * N + bj + j];
 						}
@@ -54,31 +55,63 @@ double* my_solver(int N, double *A, double* B) {
 	// (AxB)xAt
 	// A - superior triunghiulara => At - inferior triunghiulara
 	// A[i][j] = At[j][i]
-	for (i = 0; i < N; i++) {
-   		for (j = 0; j < N; j++) {
-      		for (k = j; k < N; k++) {
-				// era At[k][j] => inversez cu A[j][k]
-				// At - inferior triunghiulara
-				AxBxAt[i * N + j] += AxB[i * N + k] * A[j * N + k];
-      		}
+	// for (i = 0; i < N; i++) {
+   	// 	for (j = 0; j < N; j++) {
+    //   		for (k = j; k < N; k++) {
+	// 			// era At[k][j] => inversez cu A[j][k]
+	// 			// At - inferior triunghiulara
+	// 			AxBxAt[i * N + j] += AxB[i * N + k] * A[j * N + k];
+    //   		}
+	// 	}
+	// }
+	for (bi = 0; bi < N; bi += blockSize) {
+        for (bj = 0; bj < N; bj += blockSize) {
+            for (bk = 0; bk < N; bk += blockSize) {
+                for (i = 0; i < blockSize; i++) {
+                    for (j = 0; j < blockSize; j++) {
+						//!!!!!!!!!!!!!!!!!!!!!! vezi cum faci sa tii cont ca e sup triunghiulara
+                        for (k = 0; k < blockSize; k++) {
+							AxBxAt[(bi + i) * N + bj + j] += AxB[(bi + i) * N + bk + k] * A[(bj + j) * N + bk + k];
+						}
+					}
+				}
+			}
 		}
 	}
 
 	// Bt×Bt
 	// B[i][j] = Bt[j][i]
-	for (i = 0; i < N; i++) {
-   		for (j = 0; j < N; j++) {
-      		for (k = 0; k < N; k++) {
-				// ar fi fost de fapt Bt[i][k] * Bt[k][j]; inversez
-				BtxBt[i * N + j] += B[k * N + i] * B[j * N + k];
-      		}
+	// for (i = 0; i < N; i++) {
+   	// 	for (j = 0; j < N; j++) {
+    //   		for (k = 0; k < N; k++) {
+	// 			// ar fi fost de fapt Bt[i][k] * Bt[k][j]; inversez
+	// 			BtxBt[i * N + j] += B[k * N + i] * B[j * N + k];
+    //   		}
+	// 	}
+	// }
+
+	for (bi = 0; bi < N; bi += blockSize) {
+        for (bj = 0; bj < N; bj += blockSize) {
+            for (bk = 0; bk < N; bk += blockSize) {
+                for (i = 0; i < blockSize; i++) {
+                    for (j = 0; j < blockSize; j++) {
+                        for (k = 0; k < blockSize; k++) {
+							AxB[(bi + i) * N + bj + j] += A[(bk + k) * N + bi + i] * B[(bj + j) * N + bk + k];
+						}
+					}
+				}
+			}
 		}
 	}
 
 	// C = (AxBxAt) + (BtxBt)
+	double *orig_AxBxAt = &AxBxAt[0];
+	double *orig_BtxBt = &BtxBt[0];
 	for (i = 0; i < N; i++) {
    		for (j = 0; j < N; j++) {
-			AxBxAt[i * N + j] += BtxBt[i * N + j];
+			*orig_AxBxAt += *BtxBt;
+			orig_AxBxAt++;
+			orig_BtxBt++;
 		}
 	}
 
